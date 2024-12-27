b2acd1c1b85574104e9138573b9a52b5bf298da6
Sanish <s.karki1994@gmail.com>
2024-12-27 13:04:33 +0545

    video61- 404 Page not found added in route.js

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,37 +1,51 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom/cjs/react-router-dom';
+import {
+  Redirect,
+  Route,
+  BrowserRouter,
+  Switch,
+} from 'react-router-dom/cjs/react-router-dom';
 import MainPage from '../pages/mainPage/MainPage';
 import Login from '../pages/Login/Login';
 import AddToDo from '../pages/add-todo/AddToDo';
 import ViewPage from '../pages/ViewPage/ViewPage';
+import NotFound from '../pages/notfound/NotFound';
 
 const Routes = () => {
   // Bujne kura route ma pages hari declare gare paxi, components haru ma Routes.js import garirakhnu pardaina// direct to="/add" lekhe pugxa
   return (
     <>
-      <Route path='/' exact>
-        <Redirect to='/home' />
-      </Route>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/home' />
+          </Route>
 
-      <Route path='/home' component={MainPage} exact />
+          <Route path='/home' component={MainPage} exact />
 
-      <Route path='/add' exact>
-        {localStorage.getItem('loggedIn') ? (
-          <>
-            <AddToDo />
-          </>
-        ) : (
-          <Redirect to='/login' />
-        )}
-      </Route>
+          <Route path='/add' exact>
+            {localStorage.getItem('loggedIn') ? (
+              <>
+                <AddToDo />
+              </>
+            ) : (
+              <Redirect to='/login' />
+            )}
+          </Route>
 
-      <Route path='/login' component={Login} />
+          <Route path='/login' component={Login} />
 
-      {/* Dynamic routing */}
-      <Route path='/view/:id' component={ViewPage} exact />
+          {/* Dynamic routing */}
+          <Route path='/view/:id' component={ViewPage} exact />
 
-      {/* Query using useLocation() */}
-      {/* <Route path='/view' component={ViewPage} exact /> */}
+          <Route path='*'>
+            <NotFound />
+          </Route>
+
+          {/* Query using useLocation() */}
+          {/* <Route path='/view' component={ViewPage} exact /> */}
+        </Switch>
+      </BrowserRouter>
     </>
   );
 };
